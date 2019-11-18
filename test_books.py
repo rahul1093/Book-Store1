9e32640797711478c979d8b8d693325dc201a6e1
Rahul <vibhuterahuls@gmail.com>
2019-11-18 18:59:43 +0530

    first commit

---
+++ test_books.py
@@ -0,0 +1,14 @@
+import unittest
+from Book_on_rent import *
+
+
+class TestStringMethods(unittest.TestCase):
+
+    # Returns True if the string contains all chanters in lower case.
+    def test_isupper(self):  # testing for isupper
+        self.assertTrue('regular'.islower())
+        self.assertFalse('ReguLar'.islower())
+        self.assertTrue('fiction'.islower())
+        self.assertFalse('Fiction'.islower())
+        self.assertTrue('novel'.islower())
+        self.assertFalse('Novel'.islower())
